c77eed1b4d8cc7b4199f3c5d955ab3ff88230496
BeepBot99 <Davis.Luxenberg@outlook.com>
2023-12-31 15:26:06 -0600

    Finished code

---
+++ main.js
@@ -0,0 +1,52 @@
+const billInput = document.getElementById("bill");
+const tipButtons = document.querySelectorAll(".button-container button, .button-container input");
+const numberOfPeopleInput = document.getElementById("people");
+const tipOutput = document.getElementById("tipAmountPerPerson");
+const totalOutput = document.getElementById("totalAmountPerPerson");
+const resetButton = document.getElementById("resetButton");
+const tips = [5, 10, 15, 25, 50];
+let currentTip = 0, activeButton;
+
+function calculateTipAndTotalPerPerson(bill, tip, people) {
+  tip = Number(tip);
+    return [bill * tip / 100 / people, bill * (tip + 100) / 100 / people];
+}
+function updateValues() {
+    let tipPerPerson = calculateTipAndTotalPerPerson(billInput.value, currentTip, numberOfPeopleInput.value)[0].toFixed(2).toString();
+    let totalPerPerson = calculateTipAndTotalPerPerson(billInput.value, currentTip, numberOfPeopleInput.value)[1].toFixed(2).toString();
+    console.log(totalPerPerson);
+    tipPerPerson = isFinite(tipPerPerson) ? tipPerPerson : "0.00";
+    totalPerPerson = isFinite(totalPerPerson) ? totalPerPerson : "0.00";
+    tipOutput.innerText = "$" + tipPerPerson;
+    totalOutput.innerText = "$" + totalPerPerson;
+}
+numberOfPeopleInput.addEventListener("keyup", () => {
+    updateValues();
+});
+billInput.addEventListener("keyup", () => {
+    updateValues();
+});
+resetButton.addEventListener("click", () => {
+    billInput.value = "";
+    numberOfPeopleInput.value = "";
+    updateValues();
+});
+tipButtons.forEach(button => {
+    if (Array.from(tipButtons).indexOf(button) <= 4) {
+        button.addEventListener("click", () => {
+            try { activeButton.removeAttribute("selected") } catch (e) { };
+            activeButton = button;
+            button.setAttribute("selected", "");
+            currentTip = tips[Array.from(tipButtons).indexOf(button)];
+            tipButtons[5].value = "";
+            updateValues();
+        });
+    } else {
+        button.addEventListener("keyup", () => {
+            try { activeButton.removeAttribute("selected") } catch (e) { };
+            activeButton = undefined;
+            currentTip = button.value;
+            updateValues();
+        })
+    }
+});
